182ae98a4cfcb8db9cf22f15582b863e28bbf2fc
Yi Fei Chung <uqychun7@uq.edu.au>
2024-11-29 10:19:31 +1000

    minor updates

--- Demo_SLURM_array.R
+++ Demo_SLURM_array.R
@@ -1,11 +1,11 @@
-library(readr)
+library(tidyverse)
 
-SLURM_ARRAY_ID <- commandArgs(trailingOnly = TRUE)[1]
+Input_txt_file <- commandArgs(trailingOnly = TRUE)[1]
 
 X <- "Hello World! "
-Y <- c(101, 102, 103)
-Y <- Y[as.integer(SLURM_ARRAY_ID)]
+Y <- read_lines(Input_txt_file)
 
-XY <- paste(X, paste(Y, collapse = " "), "SLURM_ARRAY_ID = " , SLURM_ARRAY_ID)
+XY <- paste(X, Y, collapse = " ")
 
-write_lines(XY, "output_single.txt")
+FILENAME <- str_replace(Input_txt_file, "input", "output")
+write_lines(XY, FILENAME)
--- Demo_SLURM_single.R
+++ Demo_SLURM_single.R
@@ -1,7 +1,7 @@
-library(readr)
+library(tidyverse)
 
 X <- "Hello World! "
-Y <- c(101, 102, 103)
+Y <- c("It's me!", "It's me, again!", "It's me, again and again!")
 
 XY <- paste(X, paste(Y, collapse = " "))
 
